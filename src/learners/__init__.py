38c8895f742a4184ca02ce5ae7bf4fb1a6af7341
XiangqiUESTC <402214886@qq.com>
2025-06-05 16:49:30 +0800

    添加了一个学习器，修改了字典格式

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,9 +1,12 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
+from .decompose_learner import DecomposeLearner
 
-REGISTRY = {}
+REGISTRY = {
+    "q_learner": QLearner,
+    "coma_learner": COMALearner,
+    "qtran_learner": QTranLearner,
+    "decompose_learner": DecomposeLearner,
+}
 
-REGISTRY["q_learner"] = QLearner
-REGISTRY["coma_learner"] = COMALearner
-REGISTRY["qtran_learner"] = QTranLearner
